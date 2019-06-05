3925e496d79853cc3ec486c4ed1d8f679f32e16b
Vorl0r <51420958+Vorl0r@users.noreply.github.com>
2019-06-05 23:12:13 +0300

    Add files via upload

---
+++ TouchSpin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchSpin : MonoBehaviour {
+
+    public float speed = 1f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+    void OnMouseDrag() {
+        transform.Rotate(0, 0, -Input.GetAxis("Mouse X") * speed);
+    }
+    // Update is called once per frame
+	void Update () {
+        
+	}
+}
